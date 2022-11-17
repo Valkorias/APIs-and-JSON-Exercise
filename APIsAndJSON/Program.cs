32b8e475fdb943538cc078b33a3d122853843c93
David Figone <david.louis.figone@gmail.com>
2022-11-17 12:08:14 -0600

    Initial Commit

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace APIsAndJSON
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region KanyeVsRon
+            //var client = new HttpClient();
+            //var quote = new RonVSKanyeAPI(client);
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("------------------------");
+            //    Console.WriteLine($"Kanye: {quote.KanyeWest()}");
+
+            //    Console.WriteLine();
+
+            //    Console.WriteLine($"Ron Swanson: {quote.RonSwanson()}");
+            //}
+            #endregion
+
+            #region OpenWeatherMap
+            var client = new HttpClient();
+            int n, i;
+            Console.WriteLine("Please enter your API Key: ");
+            string? v1 = Console.ReadLine();
+            var key = v1;
+
+
+            string v = $"{key}";
+
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?id=4726206&appid={v1}&units=imperial";
+            //var weatherURL = $"http://api.openweathermap.org/data/2.5/forecast?id=4726206&appid={v1}&units=imperial";
+
+            var response = client.GetStringAsync(weatherURL).Result;
+            
+
+            var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+            Console.WriteLine();
+            Console.WriteLine($"The current Temperature is {JObject.Parse(formattedResponse).GetValue("temp")} degrees Fahrenheit");
+
+            //JObject formattedResponse = JObject.Parse(response);
+            //var temp = formattedResponse["list"][0]["main"]["temp"];
+            //Console.WriteLine(temp);
+            #endregion
         }
     }
-}
+} 
